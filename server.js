937372d8af498b66648022f38c9374144de4ebed
abdalla3000 <145672606+abdalla3000@users.noreply.github.com>
2024-12-24 00:43:40 +0200

    server.js

--- server.js
+++ server.js
@@ -68,3 +68,16 @@ function displayPost(post) {
 }
 
 loadPosts();
+server {
+    listen 80;
+    server_name your_domain_or_IP;
+
+    location / {
+        proxy_pass http:/crispy-blog-production.up.railway.app
+        proxy_http_version 1.1;
+        proxy_set_header Upgrade $http_upgrade;
+        proxy_set_header Connection 'upgrade';
+        proxy_set_header Host $host;
+        proxy_cache_bypass $http_upgrade;
+    }
+};
